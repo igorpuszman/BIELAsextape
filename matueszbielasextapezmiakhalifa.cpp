df663158e80fdc5cd95e41b35442a28bad5edbcb
igorpuszman <igibabes@gmail.com>
2020-04-22 23:44:15 +0200

    doom

---
+++ matueszbielasextapezmiakhalifa.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	mateusz biela
+	pokazuje
+	dupe
+	na
+	video
+	mozna
+	obejrzec
+	na
+	xhamster.spl
+	i inne slodkie
+	chomiczki
+	tam
+	sa
+	po-lecam
+	igor
+	puszman
+	return 0;
+}
